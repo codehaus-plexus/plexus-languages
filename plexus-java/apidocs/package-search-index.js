bb4de0f4093e6b98ecd1fa6789b8fb5769072b7f
Sylwester Lachiewicz <slachiewicz@gmail.com>
2023-10-16 15:43:57 +0200

    Site checkin for project Plexus Languages

---
+++ plexus-java/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.plexus.languages.java.jpms"},{"l":"org.codehaus.plexus.languages.java.version"}];updateSearchResults();
